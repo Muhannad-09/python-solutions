95c2e94a2e66b3f6075a2b689c4a99bc548f6906
Muhannad-09 <Muhannadgsgs@gmail.com>
2025-06-30 15:52:41 +0300

    launch_websites

--- web_automation/launch_websites.py
+++ web_automation/launch_websites.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-import time
+
 
 # ✅ Set options to keep Chrome open after script finishes
 options = webdriver.ChromeOptions()
